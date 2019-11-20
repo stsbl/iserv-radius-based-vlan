96c2a539871b3dd124abe3effb8313ddb70fda09
Felix Jacobi <Felix@jacobi-hamburg.net>
2019-11-20 05:39:15 +0100

    Added initial database

--- lib/chkldap.d/80radius-based-vlan.pl
+++ lib/chkldap.d/80radius-based-vlan.pl
@@ -1,16 +1,36 @@
 package chkldap::radius_based_vlan;
+
 use strict;
 use warnings;
 use IServ::DB;
 
-for my $act (IServ::DB::SelectCol "SELECT Act FROM users ORDER BY Act")
+my %users = IServ::DB::SelectAll_Hash <<SQL
+SELECT DISTINCT ON (actuser) actuser, vlan_id, priority FROM (
+  SELECT m.actuser, r1.vlan_id, r1.priority FROM radius_vlan r1
+    INNER JOIN radius_vlan_group vg ON r1.id = vg.vlan_id
+    INNER JOIN members m ON vg.group = m.actgrp
+  UNION
+  SELECT ur.act AS actuser, r2.vlan_id, r2.priority FROM radius_vlan r2
+    INNER JOIN radius_vlan_role vr ON r2.id = vr.vlan_id
+    INNER JOIN user_roles ur ON ur.role = vr.role
+  UNION
+  SELECT
+    u.act AS actuser,
+    ? AS vlan_id,
+    (SELECT MAX(r3.priority) FROM radius_vlan r3) + 1 AS priority
+  FROM users u ORDER BY priority ASC
+) AS q ORDER BY q.actuser, q.priority
+SQL
+, 123;
+
+for my $act (sort keys %users)
 {
   ::want ::dn(cn => $act, ou => "users"),
     objectClass => [
       "radiusprofile"
     ],
     radiusTunnelMediumType => "IEEE-802",
-    radiusTunnelPrivateGroupId => 123,
+    radiusTunnelPrivateGroupId => $users{$act}{vlan_id},
     radiusTunnelType => "VLAN",
   ;
 }
