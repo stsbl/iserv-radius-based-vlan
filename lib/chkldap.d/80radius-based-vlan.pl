c9ada40cfa2d0a8c4945bf3e52226602b0250326
Felix Jacobi <Felix@jacobi-hamburg.net>
2019-11-20 06:39:23 +0100

    Added some dependencies and config variable for fallback VLAN

--- lib/chkldap.d/80radius-based-vlan.pl
+++ lib/chkldap.d/80radius-based-vlan.pl
@@ -2,9 +2,30 @@ package chkldap::radius_based_vlan;
 
 use strict;
 use warnings;
+use IServ::Conf;
 use IServ::DB;
 
-my %users = IServ::DB::SelectAll_Hash <<SQL
+my $fallback_vlan_id = $conf->{RadiusBasedVlanUserFallbackId} // undef;
+
+# A VLAN ID must be between 0 (really 1 - 0 indicates disabled) and 4096
+unless ($fallback_vlan_id =~ /^[0-9]+$/ and
+    $fallback_vlan_id ge 0 and
+    $fallback_vlan_id le 4095)
+{
+  warn "Invalid value for RadiusBasedVlanUserFallbackId!\n";
+  undef $fallback_vlan_id;
+}
+
+# Disable fallback setting
+undef $fallback_vlan_id if defined $fallback_vlan_id and
+    $fallback_vlan_id eq 0;
+
+my %users;
+
+# Use matching SQL query depending on if we're using a fallback VLAN ID or not
+if (defined $fallback_vlan_id)
+{
+  %users = IServ::DB::SelectAll_Hash <<SQL
 SELECT DISTINCT ON (actuser) actuser, vlan_id, priority FROM (
   SELECT m.actuser, r1.vlan_id, r1.priority FROM radius_vlan r1
     INNER JOIN radius_vlan_group vg ON r1.id = vg.vlan_id
@@ -21,8 +42,27 @@ SELECT DISTINCT ON (actuser) actuser, vlan_id, priority FROM (
   FROM users u ORDER BY priority ASC
 ) AS q ORDER BY q.actuser, q.priority
 SQL
-, 123;
+  , $fallback_vlan_id;
+}
+else
+{
+  %users = IServ::DB::SelectAll_Hash <<SQL
+SELECT DISTINCT ON (actuser) actuser, vlan_id, priority FROM (
+  SELECT m.actuser, r1.vlan_id, r1.priority FROM radius_vlan r1
+    INNER JOIN radius_vlan_group vg ON r1.id = vg.vlan_id
+    INNER JOIN members m ON vg.group = m.actgrp
+  UNION
+  SELECT ur.act AS actuser, r2.vlan_id, r2.priority FROM radius_vlan r2
+    INNER JOIN radius_vlan_role vr ON r2.id = vr.vlan_id
+    INNER JOIN user_roles ur ON ur.role = vr.role
+) AS q ORDER BY q.actuser, q.priority
+SQL
+  ;
+}
 
+# Add radiusProfile to all IServ users which have a VLAN ID assigned. If there
+# is not explicit VLAN set and we do not have a fallback, "unknown" will not
+# get the object "radiusprofile".
 for my $act (sort keys %users)
 {
   ::want ::dn(cn => $act, ou => "users"),
