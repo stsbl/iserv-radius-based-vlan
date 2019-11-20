e222871ebe8ba781930705151ad188337aeb837e
Felix Jacobi <Felix@jacobi-hamburg.net>
2019-11-20 08:25:34 +0100

    Added support for setting room and IP range per VLAN (hacky)

--- db/radius-based-vlan.sql
+++ db/radius-based-vlan.sql
@@ -2,7 +2,11 @@ CREATE TABLE radius_vlan (
 	ID		SERIAL		PRIMARY KEY,
 	Description	TEXT		NOT NULL,
 	VLAN_ID		INT		NOT NULL CHECK (VLAN_ID BETWEEN 1 AND 4095),
-	PRIORITY	INT		NOT NULL UNIQUE DEFAULT 0
+	Priority	INT		NOT NULL UNIQUE DEFAULT 0,
+	Room_ID		INT		REFERENCES rooms(ID)
+						ON UPDATE CASCADE
+						ON DELETE SET NULL,
+	IP_Range	INET
 );
 
 CREATE TABLE radius_vlan_group (
@@ -11,8 +15,11 @@ CREATE TABLE radius_vlan_group (
 						ON DELETE CASCADE,
 	Group		TEXT		NOT NULL REFERENCES groups(Act)
 						ON UPDATE CASCADE
-						ON DELETE CASCADE
+						ON DELETE CASCADE,
+	PRIMARY KEY (VLAN_ID, Group)
 );
+
+CREATE INDEX radius_vlan_group_vlan_id_key ON radius_vlan_group(VLAN_ID);
 
 CREATE TABLE radius_vlan_role (
 	VLAN_ID		INT		NOT NULL REFERENCES radius_vlan(ID)
@@ -20,5 +27,8 @@ CREATE TABLE radius_vlan_role (
 						ON DELETE CASCADE,
 	Role		TEXT		NOT NULL REFERENCES security_roles(Role)
 						ON UPDATE CASCADE
-						ON DELETE CASCADE
+						ON DELETE CASCADE,
+	PRIMARY KEY (VLAN_ID, Role)
 );
+
+CREATE INDEX radius_vlan_role_vlan_id_key ON radius_vlan_role(VLAN_ID);
---
+++ db/wlan_radius-based-vlan.sql
@@ -0,0 +1,1 @@
+GRANT SELECT ON radius_vlan, radius_vlan_group, radius_vlan_role, user_roles, members TO wlan;
