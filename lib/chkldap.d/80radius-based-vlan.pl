a1353a167685e735d5254493bfe815419ce3feb1
Felix Jacobi <Felix@jacobi-hamburg.net>
2019-11-21 08:45:10 +0100

    Added secondary FreeRADIUS server for MAC-based VLAN assignment

--- lib/chkldap.d/80radius-based-vlan.pl
+++ lib/chkldap.d/80radius-based-vlan.pl
@@ -5,14 +5,27 @@ use warnings;
 use IServ::Conf;
 use IServ::DB;
 
-my $fallback_vlan_id = $conf->{RadiusBasedVlanUserFallbackId} // undef;
+sub mac_to_cases($)
+{
+  my ($mac) = @_;
+  my @out;
+  $mac = lc $mac;
+
+  push @out, $mac;
+  push @out, ($mac =~ s/://gr);
+  push @out, ($mac =~ s/:/-/gr);
+
+  @out;
+}
+
+my $fallback_vlan_id = $conf->{RadiusBasedVlanFallbackId} // undef;
 
 # A VLAN ID must be between 0 (really 1 - 0 indicates disabled) and 4096
 unless ($fallback_vlan_id =~ /^[0-9]+$/ and
     $fallback_vlan_id ge 0 and
     $fallback_vlan_id le 4095)
 {
-  warn "Invalid value for RadiusBasedVlanUserFallbackId!\n";
+  warn "Invalid value for RadiusBasedVlanFallbackId!\n";
   undef $fallback_vlan_id;
 }
 
@@ -20,7 +33,7 @@ unless ($fallback_vlan_id =~ /^[0-9]+$/ and
 undef $fallback_vlan_id if defined $fallback_vlan_id and
     $fallback_vlan_id eq 0;
 
-my %users;
+my (%hosts, %users);
 
 # Use matching SQL query depending on if we're using a fallback VLAN ID or not
 if (defined $fallback_vlan_id)
@@ -43,6 +56,22 @@ SELECT DISTINCT ON (actuser) actuser, vlan_id, priority FROM (
 ) AS q ORDER BY q.actuser, q.priority
 SQL
   , $fallback_vlan_id;
+  %hosts = IServ::DB::SelectAll_Hash <<SQL
+SELECT DISTINCT ON (name) name, mac, vlan_id, priority FROM (
+  SELECT h.name, h.mac, r1.vlan_id, r1.priority FROM radius_vlan r1
+    RIGHT JOIN hosts h ON h.ip << r1.ip_range
+  WHERE h.mac IS NOT NULL
+  UNION
+  SELECT
+    h2.name,
+    h2.mac,
+    ? AS vlan_id,
+    (SELECT MAX(r3.priority) FROM radius_vlan r3) + 1 AS priority
+  FROM hosts h2
+  WHERE h2.mac IS NOT NULL
+) AS q ORDER BY q.name, q.priority
+SQL
+  , $fallback_vlan_id;
 }
 else
 {
@@ -58,11 +87,19 @@ SELECT DISTINCT ON (actuser) actuser, vlan_id, priority FROM (
 ) AS q ORDER BY q.actuser, q.priority
 SQL
   ;
+  %hosts = IServ::DB::SelectAll_Hash <<SQL
+SELECT DISTINCT ON (name) name, mac, vlan_id, priority FROM (
+  SELECT h.name, h.mac, r1.vlan_id, r1.priority FROM radius_vlan r1
+    RIGHT JOIN hosts h ON h.ip << r1.ip_range
+  WHERE h.mac IS NOT NULL
+) AS q ORDER BY q.name, q.priority
+SQL
+  ;
 }
 
 # Add radiusProfile to all IServ users which have a VLAN ID assigned. If there
-# is not explicit VLAN set and we do not have a fallback, "unknown" will not
-# get the object "radiusprofile".
+# is not explicit VLAN set and we do not have a fallback, "unknown" users will
+# not get the object "radiusprofile".
 for my $act (sort keys %users)
 {
   ::want ::dn(cn => $act, ou => "users"),
@@ -74,5 +111,47 @@ for my $act (sort keys %users)
     radiusTunnelType => "VLAN",
   ;
 }
+
+::want ::dn(ou => "hosts"),
+  objectClass => [ "organizationalUnit" ],
+  ou => "hosts"
+;
+
+# Add simpleSecurityObject with radiusProfile to all hosts which have a VLAN ID
+# assigned. If there is not explicit VLAN set and we do not have a fallback,
+# "unknown" hosts will not listed here
+for my $name (sort keys %hosts)
+{
+  for my $mac (mac_to_cases $hosts{$name}{mac})
+  {
+    unless (defined $hosts{$name}{vlan_id})
+    {
+      ::want ::dn(cn => $mac, ou => "hosts"),
+        cn => $mac,
+        objectClass => [
+          "simpleSecurityObject",
+          "organizationalRole"
+        ],
+        userPassword => "{crypt}!"
+      ;
+
+      next;
+    }
+
+    ::want ::dn(cn => $mac, ou => "hosts"),
+      cn => $mac,
+      objectClass => [
+        "radiusprofile",
+        "simpleSecurityObject",
+        "organizationalRole"
+      ],
+      radiusTunnelMediumType => "IEEE-802",
+      radiusTunnelPrivateGroupId => $hosts{$name}{vlan_id},
+      radiusTunnelType => "VLAN",
+      userPassword => "{crypt}!"
+    ;
+  }
+}
+
 
 1;
